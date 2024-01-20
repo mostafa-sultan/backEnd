9c2cc2b0cd4eef11a60e561b295e9fa55c2def97
mostafa-sultan <mostafasoltan82@gmail.com>
2024-01-20 08:45:07 +0200

    add todoRoutes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./src/routes/authRoutes');
- 
+const todoRoutes = require('./src/routes/todoRoutes');
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(authRoutes);
- 
+app.use(todoRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
